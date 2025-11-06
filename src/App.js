a1602f1b3fc5319026ce89f063e1645073f1e56c
shimul <darkegale3536@gmail.com>
2025-11-06 17:39:31 +0600

    update

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import './App.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import NavbarEffect from './Components/navbar';
+import Home  from './Page/home'
+import Services from "./Page/services";
+import Project from "./Page/project";
+import Contact from "./Page/contact";
+import Notfound from "./Page/notfound";
+
+
+function App() {
+  const navLinks = [
+    { name: "Home", href: "/" },
+    { name: "Services", href: "/services" },
+    { name: "Projects", href: "/project" },
+    { name: "Contact", href: "/contact" },
+  ];
+
+  return (
+    <>
+    <BrowserRouter>
+      <NavbarEffect links={navLinks}/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/home" element={<Home/>}/>
+        <Route path="/project" element={<Project/>}/>
+        <Route path="/services" element={<Services/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+        <Route path="*" element={<Notfound/>}/>
+      </Routes>
+    </BrowserRouter>
+    </>
+  )
+}
+
+export default App;
+
---
+++ src/Page/project.jsx
@@ -0,0 +1,13 @@
+
+import Cards from "../Components/cards";
+import '../styles/project.css'
+export default function Project(){
+    return(
+        <section id="project">
+            <Cards
+            className="project_cards"
+            title="This is a cards"
+            />
+        </section>
+    )
+}
