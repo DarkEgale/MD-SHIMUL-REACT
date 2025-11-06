1f5894f13a5f584ca29ccc7702101f290c629140
shimul <darkegale3536@gmail.com>
2025-11-06 18:02:20 +0600

    fix

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, HashRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import NavbarEffect from './Components/navbar';
-import Home  from './Page/home'
+import Home from './Page/home';
 import Services from "./Page/services";
 import Project from "./Page/project";
 import Contact from "./Page/contact";
 import Notfound from "./Page/notfound";
-
 
 function App() {
   const navLinks = [
@@ -17,9 +16,12 @@ function App() {
     { name: "Contact", href: "/contact" },
   ];
 
+
+  const Router =
+    process.env.NODE_ENV === "production" ? HashRouter : BrowserRouter;
+
   return (
-    <>
-    <BrowserRouter>
+    <Router>
       <NavbarEffect links={navLinks}/>
       <Routes>
         <Route path="/" element={<Home/>}/>
@@ -29,10 +31,8 @@ function App() {
         <Route path="/contact" element={<Contact/>}/>
         <Route path="*" element={<Notfound/>}/>
       </Routes>
-    </BrowserRouter>
-    </>
-  )
+    </Router>
+  );
 }
 
 export default App;
-
